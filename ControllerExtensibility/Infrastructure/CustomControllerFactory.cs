2a320170a0b36c12a1d6b199a1ae91b5ef0fe8f9
JeanSchwalenberg <jeanschwalenberg@gmail.com>
2016-06-15 14:52:35 -0600

    + CustomControllerFactory.cs

---
+++ ControllerExtensibility/Infrastructure/CustomControllerFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.SessionState;
+using ControllerExtensibility.Controllers;
+
+namespace ControllerExtensibility.Infrastructure {
+    public class CustomControllerFactory {
+    }
+}
