84555ff3771a264757f3e082c17aa0e818edc612
JeanSchwalenberg <jeanschwalenberg@gmail.com>
2016-06-15 19:35:22 -0600

    finished writing CustomControllerFactory
    added ControllerBuilder reference or call or something to Global.asax

--- ControllerExtensibility/Global.asax.cs
+++ ControllerExtensibility/Global.asax.cs
@@ -4,15 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using ControllerExtensibility.Infrastructure;
 
-namespace ControllerExtensibility
-{
-    public class MvcApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
+namespace ControllerExtensibility {
+
+    public class MvcApplication : System.Web.HttpApplication {
+
+        protected void Application_Start() {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            ControllerBuilder.Current.SetControllerFactory(new
+                CustomControllerFactory());
         }
     }
 }
--- ControllerExtensibility/Infrastructure/CustomControllerFactory.cs
+++ ControllerExtensibility/Infrastructure/CustomControllerFactory.cs
@@ -7,6 +7,41 @@ using System.Web.SessionState;
 using ControllerExtensibility.Controllers;
 
 namespace ControllerExtensibility.Infrastructure {
-    public class CustomControllerFactory {
+
+    public class CustomControllerFactory : IControllerFactory {
+
+        public IController CreateController(RequestContext requestContext,
+            string controllerName) {
+
+            Type targetType = null;
+            switch (controllerName) {
+                case "Product":
+                    targetType = typeof(ProductController);
+                    break;
+                case "Customer":
+                    targetType = typeof(CustomerController);
+                    break;
+                default:
+                    requestContext.RouteData.Values["controller"] = "Product";
+                    targetType = typeof(ProductController);
+                    break;
+            }
+
+            return targetType == null ? null :
+                (IController)DependencyResolver.Current.GetService(targetType);
+        }
+
+        public SessionStateBehavior GetControllerSessionBehavior(RequestContext
+            requestContext, string controllerName) {
+
+            return SessionStateBehavior.Default;
+        }
+
+        public void ReleaseController(IController controller) {
+            IDisposable disposable = controller as IDisposable;
+            if (disposable != null) {
+                disposable.Dispose();
+            }
+        }
     }
 }
